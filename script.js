0584fd9661904e8460765ed8e8b6272bb8def93c
Utkarsh Kumar Upadhyay <136082162+utkarshupdy@users.noreply.github.com>
2024-05-30 13:49:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+const daysElement = document.querySelector(".days")
+const hourElement = document.querySelector(".hours")
+const minuteElement = document.querySelector(".minutes")
+const secondElement = document.querySelector(".seconds")
+const heading = document.querySelector("h1")
+const counterTimer = document.querySelector(".counterTimer")
+
+
+const second = 1000;
+const minute = 60 *second
+const hour = 60*minute
+const day = 24*hour
+
+const timerFunction = ()=>{
+    let now = new Date();
+    let dd = String(now.getDate()).padStart(2 , "0");
+    let mm = String(now.getMonth()+1).padStart(2 , "0");
+    let yyyy = now.getFullYear()
+
+    const enteredDay = prompt("Enter Day").padStart(2 , "0")
+    const enteredMonth = prompt("Enter Month").padStart(2 , "0")
+    // const enteredYear = prompt("Enter Year").padStart(2 , "0")
+
+    let targetDate = `${enteredMonth}/${enteredDay}/${yyyy}`;
+
+    now = `${mm}/${dd}/${yyyy}`;
+    if(now > targetDate){
+        targetDate = `${enteredMonth}/${enteredDay}/${yyyy + 1}`;
+
+    }
+    const intervalID = setInterval(() => {
+        const timer = new Date(targetDate).getTime()
+        const today = new Date().getTime();
+
+        const difference = timer - today
+        const leftDay = Math.floor(difference/day)
+        const leftHour = Math.floor((difference % day) / hour)
+        const leftMinute = Math.floor((difference % hour) / minute)
+        const leftSecond = Math.floor((difference % minute) / second)
+
+        daysElement.innerText = leftDay ;
+        hourElement.innerText = leftHour;
+        minuteElement.innerText = leftMinute;
+        secondElement.innerText = leftSecond;
+        if(difference < 0){
+            counterTimer.style.display = "none"
+            heading.innerText = "Time's Up";
+            clearInterval(intervalID);
+        }
+
+        
+    }, 0);
+
+}
+timerFunction()
